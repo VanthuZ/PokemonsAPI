be5e02db4f1c17276426e95208e6d7d865062b3a
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-08-18 20:12:04 +0200

    init commit

---
+++ src/test/java/pl/vanthus/pokemonsAPI/PokemonsApiApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.vanthus.pokemonsAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PokemonsApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
